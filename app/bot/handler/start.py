f43b96f60aa91648ef7a0bfc6cd4e379562dea3e
bizmohammad <mohammad.arif@bizmetric.com>
2024-06-16 21:03:38 +0530

    code updated

--- app/bot/handler/start.py
+++
@@ -1,6 +0,0 @@
-from aiogram.types import Message
-from aiogram.filters import CommandStart
-
-@dp.message(CommandStart())
-async def welcome(message: Message):
-    await message.answer("Hello! \nI'm AI Powered Telegram bot created by Mohammad!\nHow may I help you with your queries related to BERT paper?")
